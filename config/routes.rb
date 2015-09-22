8511a9e497952b8251dd9302a89d74860c0408e3
janowskipio <janowskipio@gmail.com>
2015-09-22 11:52:23 +0200

    user authentication v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root 'todo_lists#index'
 
+  get 'login' => 'users#login', :as => 'login'
+  post 'login' => 'users#attempt_login', :as => 'create_user'
+  get 'logout' => 'users#logout', :as => 'logout'
+
   resources :todo_lists do
     resources :todo_tasks
   end
 
-  match ':controller(/:action(/:id))', :via => [:get, :post]
   resources :users
 
 end
